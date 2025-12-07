c542f84ec1d079e50cd69a2715be7ceeab5cfd9a
lifeisgenie <tmdwls2437@gmail.com>
2025-12-07 08:15:25 +0000

    chore: add Spring Boot entrypoints for all services

---
+++ erp-approval-processing-service/src/main/java/com/example/erp/approvalprocessing/ApprovalProcessingServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.erp.approvalprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprovalProcessingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApprovalProcessingServiceApplication.class, args);
+    }
+}
---
+++ erp-approval-request-service/src/main/java/com/example/erp/approvalrequest/ApprovalRequestServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.erp.approvalrequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprovalRequestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApprovalRequestServiceApplication.class, args);
+    }
+}
